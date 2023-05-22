10b845248ebbfeeb976c1383e6533b7a8223127b
s1c <65496215+s1csty9@users.noreply.github.com>
2023-05-22 19:22:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import subprocess
 import os
+import requests
+import time
 
 user = os.getlogin()
 insdir = f'/home/{user}/Morpheus/'
+downloadurl = 'https://github.com/s1csty9/Morpheus/raw/main/download.py'
+savescrpath = f'{insdir}download.py'
+
 if os.path.exists(insdir)and os.path.isdir(insdir):
- print('ok')
+ response = requests.get(downloadurl)
+ if response.status_code == 200:
+    with open(savescrpath, 'wb') as file:
+        file.write(response.content)
+        #while not os.path.exists(f'{insdir}download.py'):
+            #time.sleep(0.1)
+        subprocess.run(['python3', f'{insdir}download.py'])
 else:
   subprocess.run(['mkdir', '-p', insdir])
-#insname will be set to different file names depending on user input in the future, for now ins_tor.sh is a placeholder for testing
-insname='ins_tor.sh'
-script_path=f'{insdir}{insname}'
 
-
-subprocess.run(['bash', script_path])
