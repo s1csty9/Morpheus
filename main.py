15891f795917cffaaa79009196e2e86f6c19f6ac
s1c <65496215+s1csty9@users.noreply.github.com>
2023-05-22 17:20:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if os.path.exists(insdir)and os.path.isdir(insdir):
  print('ok')
 else:
   subprocess.run(['mkdir', '-p', insdir])
-
+#insname will be set to different file names depending on user input in the future, for now ins_tor.sh is a placeholder for testing
 insname='ins_tor.sh'
 script_path=f'{insdir}{insname}'
 
