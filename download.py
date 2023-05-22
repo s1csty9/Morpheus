4e16d2a5d835da379122bc9d0bed5bb7abec127d
s1c <65496215+s1csty9@users.noreply.github.com>
2023-05-22 19:26:26 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,1 +1,12 @@
 #I made this to not clutter up main.py, I probably could've done it better but DEAL WITH IT
+import requests
+import os
+
+user = os.getlogin()
+insdir = f'/home/{user}/Morpheus/'
+downloadgui = 'https://raw.githubusercontent.com/s1csty9/Morpheus/main/gui.py'
+saveguipath = f'{insdir}gui.py'
+response = requests.get(downloadgui)
+ if response.status_code == 200:
+    with open(savegipath, 'wb') as file:
+        file.write(response.content)
