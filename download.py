56f98706a1a2b19bcd66c138c617a36179943e15
s1c <65496215+s1csty9@users.noreply.github.com>
2023-05-22 19:33:39 +0100

    Update download.py

--- download.py
+++ download.py
@@ -7,6 +7,6 @@ insdir = f'/home/{user}/Morpheus/'
 downloadgui = 'https://raw.githubusercontent.com/s1csty9/Morpheus/main/gui.py'
 saveguipath = f'{insdir}gui.py'
 response = requests.get(downloadgui)
- if response.status_code == 200:
+if response.status_code == 200:
     with open(saveguipath, 'wb') as file:
         file.write(response.content)
