c87964d9cb8899f9934d888cd53b8199af76cb35
s1c <65496215+s1csty9@users.noreply.github.com>
2023-05-25 17:10:18 +0100

    Update download.py

--- download.py
+++ download.py
@@ -7,15 +7,16 @@ insdir = f'/home/{user}/Morpheus/'
 downloadgui = 'https://raw.githubusercontent.com/s1csty9/Morpheus/main/gui.py'
 saveguipath = f'{insdir}gui.py'
 downloadid = 0
-downloadidfile = f'{downloadid}.sh'
-downloadidurl = f'https://raw.githubusercontent.com/s1csty9/Morpheus/main/install_scripts/install_{downloadid}.sh'
 response = requests.get(downloadgui)
-response2_idfk = requests.get(downloadidurl)
+
 if response.status_code == 200:
     with open(saveguipath, 'wb') as file:
         file.write(response.content)
 
 while downloadid < 4:
+    downloadidfile = f'{downloadid}.sh'
+    downloadidurl = f'https://raw.githubusercontent.com/s1csty9/Morpheus/main/install_scripts/install_{downloadid}.sh'
+    response2_idfk = requests.get(downloadidurl)
     if response2_idfk.status_code == 200:
       with open(downloadidfile, 'wb') as file:
          file.write(response2_idfk.content)
