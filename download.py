476298c0ce4bcf022162e8c2dcb774e53ee2e768
s1c <65496215+s1csty9@users.noreply.github.com>
2023-05-25 17:02:44 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
-#I made this to not clutter up main.py, I probably could've done it better but DEAL WITH IT
+#woo! optimisation! I'm a fucking genius!
 import requests
 import os
 
@@ -6,7 +6,17 @@ user = os.getlogin()
 insdir = f'/home/{user}/Morpheus/'
 downloadgui = 'https://raw.githubusercontent.com/s1csty9/Morpheus/main/gui.py'
 saveguipath = f'{insdir}gui.py'
+downloadid = 0
+downloadidfile = f'{downloadid}.sh'
+downloadidurl = f'https://raw.githubusercontent.com/s1csty9/Morpheus/main/install_scripts/install_{downloadid}.sh'
 response = requests.get(downloadgui)
+response2_idfk = requests.get(downloadidurl)
 if response.status_code == 200:
     with open(saveguipath, 'wb') as file:
         file.write(response.content)
+
+while downloadid < 4:
+    if response2_idfk.status_code == 200:
+      with open(downloadidfile, 'wb') as file:
+         file.write(response2_idfk.content)
+    downloadid = downloadid+1
