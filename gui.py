6102613819f04260d45a2287d5dc883c6057273b
s1c <65496215+s1csty9@users.noreply.github.com>
2023-05-26 23:19:04 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,10 +1,3 @@
 #this will eventually be used for the gui
 #you can ignore everything here
 
-print('it works')
-#user = os.getlogin()
-#insdir = f'/home/{user}/Morpheus/'
-#insname='ins_.sh'
-#script_path=f'{insdir}{insname}'
-
-#subprocess.run(['bash', script_path])
